a9bcdde9a77d50aa4aadc6aee32cae6cdfd7254f
Heitor <heitoraugusto10@hotmail.com>
2019-06-08 02:33:57 -0300

    Visual da tela de listagem de oficinas e agendamento
    Construção do visual de listagem, melhor uso de espaço sobre o item da oficina em agendamentos, ajuste no navbar para melhor visualização

--- src/app/lista-oficina/lista-oficina.module.ts
+++ src/app/lista-oficina/lista-oficina.module.ts
@@ -1,13 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatIconModule, MatListModule, MatCardModule, MatButtonModule,
-  MatTooltipModule, MatSelectModule} from '@angular/material';
+  MatTooltipModule, MatSelectModule, MatGridListModule, MatFormFieldModule} from '@angular/material';
 import { ListaOficinaComponent } from './lista-oficina.component';
 import { AppComponent } from 'src/app/app.component';
 import { OficinaService } from 'src/app/shared/oficina.service';
 import { ListaOficinaRoutingModule } from './lista-oficina.routing';
 import { LocalSaveService } from '../shared/local-save.service';
 import { ListaOficinaGuard } from '../guards/lista-oficina.guard';
+import { ReactiveFormsModule } from '@angular/forms';
 
 
 @NgModule({
@@ -20,6 +21,9 @@ import { ListaOficinaGuard } from '../guards/lista-oficina.guard';
     MatButtonModule,
     MatTooltipModule,
     MatSelectModule,
+    MatGridListModule,
+    MatFormFieldModule,
+    ReactiveFormsModule
   ],
   declarations: [
     ListaOficinaComponent,
