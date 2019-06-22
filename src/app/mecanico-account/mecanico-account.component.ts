bb76230249bbb4b74b4298560ab02f7ed8ed0687
nicolas-sss <nicolas.moura@ufrpe.br>
2019-06-22 17:00:20 -0300

    melhorias pontuais e testes finais da versão

--- src/app/mecanico-account/mecanico-account.component.ts
+++ src/app/mecanico-account/mecanico-account.component.ts
@@ -24,7 +24,6 @@ export class MecanicoAccountComponent implements OnInit {
     private readonly route: ActivatedRoute, private _location: Location) { }
 
   ngOnInit() {
-    console.log(this.route.snapshot.params.id);
     this.id = this.route.snapshot.params.id;
     this.mecanicoService.getMecanicoById(this.id).subscribe({
       next: resp => {
